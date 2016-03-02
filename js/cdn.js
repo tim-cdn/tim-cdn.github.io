c638734f2d771aee9020100882b72429250303ef
tim-cdn <developer.timsayers@gmail.com>
2016-03-02 15:03:35 +1100

    Update cdn.js

--- js/cdn.js
+++ js/cdn.js
@@ -1,1 +1,8 @@
 //CDN.js
+function hide(id){
+  alert("hidden!")
+        document.getElementById(id).hidden = true;
+    }
+function show(id){
+        document.getElementById(id).hidden = false;
+    }
